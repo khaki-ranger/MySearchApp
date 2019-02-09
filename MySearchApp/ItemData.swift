f6e9dabfa06e08c42fcf94155bf05fd36f1fade5
Terashima <yamanogomora@gmail.com>
2019-02-09 18:03:51 +0900

    商品データを管理するItemDataクラスを実装

---
+++ MySearchApp/ItemData.swift
@@ -0,0 +1,23 @@
+//
+//  ItemData.swift
+//  MySearchApp
+//
+//  Created by 寺島 洋平 on 2019/02/09.
+//  Copyright © 2019年 YoheiTerashima. All rights reserved.
+//
+
+import Foundation
+
+class ItemData {
+    // 商品画像のURL
+    var itemImageUrl: String?
+    
+    // 商品のタイトル
+    var itemTitle: String?
+    
+    // 商品の価格
+    var itemPrice: String?
+    
+    // 商品詳細のWebページのURL
+    var itemUrl: String?
+}
